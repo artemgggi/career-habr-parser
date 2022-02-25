5b3639ea42268e32adc45e0c4b6b749370d7efaa
Artem Ivanchikov <artem.ivanchikov.99@mail.ru>
2022-02-25 16:03:06 +0300

    create sheduler

---
+++ src/main/java/AlertRabbit.java
@@ -0,0 +1,51 @@
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import java.util.ArrayList;
+import java.util.List;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        try {
+            List<Long> store = new ArrayList<>();
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDataMap data = new JobDataMap();
+            data.put("store", store);
+            JobBuilder.newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(5)
+                    .repeatForever();
+            Trigger trigger = new Trigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+            Thread.sleep(5000);
+            scheduler.shutdown();
+            System.out.println(store);
+        } catch (SchedulerException | InterruptedException se) {
+            se.printStackTrace();
+        }
+    }
+
+
+    public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
+        @Override
+        public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+            System.out.println("Rabbit runs here...");
+            List<Long> store = (List<Long>) jobExecutionContext
+                    .getJobDetail()
+                    .getJobDataMap()
+                    .get("store");
+            store.add(System.currentTimeMillis());
+        }
+    }
+}
