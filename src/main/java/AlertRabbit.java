12212d27b819cd4d9c7b225f1a48455a8bee6a0f
Artem <artem.ivanchikov.99@mail.ru>
2022-03-30 22:25:52 +0600

    Added readConfig method

--- src/main/java/AlertRabbit.java
+++ src/main/java/AlertRabbit.java
@@ -2,46 +2,57 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import java.util.ArrayList;
 import java.util.List;
-import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
-import static org.quartz.TriggerBuilder.newTrigger;
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
+    public AlertRabbit() {
+    }
+
     public static void main(String[] args) {
         try {
+            String path = "src/main/resources/rabbit.properties";
             List<Long> store = new ArrayList<>();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDataMap data = new JobDataMap();
             data.put("store", store);
-            JobBuilder.newJob(Rabbit.class)
+            JobDetail job = newJob(Rabbit.class)
                     .usingJobData(data)
                     .build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(5)
+                    .withIntervalInSeconds(readConfig(path))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
-            Thread.sleep(5000);
+            scheduler.scheduleJob(job, trigger);
+            Thread.sleep(10000);
             scheduler.shutdown();
             System.out.println(store);
-        } catch (SchedulerException | InterruptedException se) {
+        } catch (Exception se) {
             se.printStackTrace();
         }
     }
 
+    public static int readConfig(String path) {
+        ru.job4j.io.Config config = new ru.job4j.io.Config(path);
+        config.load();
+        return Integer.parseInt(config.value("rabbit.interval"));
+    }
 
     public static class Rabbit implements Job {
 
         public Rabbit() {
             System.out.println(hashCode());
         }
-
         @Override
-        public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-            System.out.println("Rabbit runs here...");
-            List<Long> store = (List<Long>) jobExecutionContext
+        public void execute(JobExecutionContext context)
+                throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+            List<Long> store = (List<Long>) context
                     .getJobDetail()
                     .getJobDataMap()
                     .get("store");
