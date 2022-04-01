5a1fd1929e18aebc5f473bb666949f357057cbf1
Artem <artem.ivanchikov.99@mail.ru>
2022-04-01 11:18:56 +0600

    create method for sql operations

--- src/main/java/AlertRabbit.java
+++ src/main/java/AlertRabbit.java
@@ -1,9 +1,6 @@
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import static org.quartz.JobBuilder.*;
@@ -14,18 +11,7 @@ public class AlertRabbit {
 
     public static void main(String[] args) {
         try {
-            Class.forName("org.postgresql.Driver");
-            String url = "jdbc:postgresql://localhost:5432/aggregator";
-            String login = "postgres";
-            String password = "password";
-            try (Connection connection = DriverManager.getConnection(url, login, password)) {
-                DatabaseMetaData metaData = connection.getMetaData();
-                System.out.println("Connection database successfully");
-                System.out.println(metaData.getUserName());
-                System.out.println(metaData.getURL());
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            writeRunTimeSql();
             String path = "src/main/resources/rabbit.properties";
             List<Long> store = new ArrayList<>();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
@@ -57,10 +43,30 @@ public class AlertRabbit {
         return Integer.parseInt(config.value("rabbit.interval"));
     }
 
+    public static void writeRunTimeSql() throws ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/aggregator";
+        String login = "postgres";
+        String password = "password";
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            System.out.println("Connection database successfully");
+            try (Statement statement = connection.createStatement()) {
+                String addTimeQuery = "INSERT INTO logs.rabbit(id, created_date)"
+                        + "VALUES(3, 'date_test3')";
+                statement.execute(addTimeQuery);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static class Rabbit implements Job {
 
         public Rabbit() {
-            System.out.println(hashCode());
+            System.out.println("Hash code " + hashCode());
         }
         @Override
         public void execute(JobExecutionContext context) {
